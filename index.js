2acfa026cc687ca9028985531e47a3027c36f552
Vivian Yim <keroviv86@gmail.com>
2021-12-13 19:24:36 -0600

    commit tree 15656188d9aefbe9fb3b90cd645f9c78b5f767a0

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31
+let num2 = 2
 
-// Write your code here
+const multiply = num1 * num2
+
+
+let random = Math.ceil(Math.random()*10)
+
+let num3 =16 
+let num4 =4
+const mod = num3/num4
+
+
+const max = Math.max(1,4,5,20)
